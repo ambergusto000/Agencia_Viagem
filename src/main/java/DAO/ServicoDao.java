a222ace69f8745aa09043c7071385544ca462025
paulosrng <paulosrngbusiness@gmail.com>
2025-04-24 17:33:11 -0300

    ServicoDao

---
+++ src/main/java/DAO/ServicoDao.java
@@ -0,0 +1,50 @@
+package DAO;
+
+import model.ServicoAdicional;
+import util.DB;
+import java.sql.*;
+import java.util.*;
+
+public class ServicoDAO {
+    public void adicionar(ServicoAdicional s) throws SQLException {
+        String sql = "INSERT INTO servicos (nome, descricao, preco) VALUES (?, ?, ?)";
+        try (Connection conn = DB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, s.getNome());
+            stmt.setString(2, s.getDescricao());
+            stmt.setDouble(3, s.getPreco());
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<ServicoAdicional> listar() throws SQLException {
+        List<ServicoAdicional> lista = new ArrayList<>();
+        String sql = "SELECT * FROM servicos";
+        try (Connection conn = DB.getConnection(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                ServicoAdicional s = new ServicoAdicional();
+                s.setId(rs.getInt("id"));
+                s.setNome(rs.getString("nome"));
+                s.setDescricao(rs.getString("descricao"));
+                s.setPreco(rs.getDouble("preco"));
+                lista.add(s);
+            }
+        }
+        return lista;
+    }
+
+    public List<ServicoAdicional> listarTodos() throws SQLException {
+        List<ServicoAdicional> lista = new ArrayList<>();
+        String sql = "SELECT * FROM servicos";
+        try (Connection conn = DB.getConnection(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                ServicoAdicional s = new ServicoAdicional();
+                s.setId(rs.getInt("id"));
+                s.setNome(rs.getString("nome"));
+                s.setDescricao(rs.getString("descricao"));
+                s.setPreco(rs.getDouble("preco"));
+                lista.add(s);
+            }
+        }
+        return lista;
+    }
+}
